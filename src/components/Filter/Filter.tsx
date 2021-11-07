ac2342eb44b2ec7fee2e683c72160b22ad7964b5
Kevin Baik <mphist@gmail.com>
2021-11-07 17:16:13 -0500

    Correct TypeFilter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,5 @@
 import { css } from '@mui/styled-engine'
+import SizeFilter from '../SizeFilter/SizeFilter'
 import TypeFilter from '../TypeFilter/TypeFilter'
 
 export type FilterProps = {}
@@ -8,6 +9,7 @@ function Filter({}: FilterProps) {
     <section css={filterHeader}>
       <div css={filterHeaderLeft}>
         <TypeFilter />
+        <SizeFilter />
       </div>
       <div css={filterHeaderRight}></div>
     </section>
@@ -17,19 +19,57 @@ function Filter({}: FilterProps) {
 const filterHeaderLeft = css`
   display: flex;
 
-  .MuiOutlinedInput-root {
+  /* .MuiOutlinedInput-root {
     border-radius: 0;
+    :hover {
+      border: 1px solid black;
+      margin: 0 -1px;
+    }
   }
   .Mui-focused {
     color: black !important;
     border: 1px solid black;
+    margin: 0 -1px;
   }
   .MuiInputLabel-outlined {
     border: none;
   }
   .MuiOutlinedInput-notchedOutline {
     border: none;
+  } */
+  .MuiFormControl-root {
+    flex-direction: row;
+    label {
+      position: relative;
+      color: black;
+    }
   }
+  .MuiOutlinedInput-root {
+    all: unset;
+    height: 3rem;
+    width: 6rem;
+    position: relative;
+    right: 2rem;
+  }
+  .Mui-focused {
+  }
+  .MuiInputLabel-outlined {
+    all: unset;
+    position: relative;
+    top: 0.7rem;
+    left: 1.5rem;
+  }
+  .MuiOutlinedInput-notchedOutline {
+    all: unset;
+  }
+  .MuiSelect-iconOpen {
+  }
+  /* .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input {
+    padding: 0;
+  }
+  .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input {
+    padding: 0;
+  } */
 `
 const filterHeaderRight = css``
 
--- src/components/TypeFilter/TypeFilter.tsx
+++ src/components/TypeFilter/TypeFilter.tsx
@@ -1,18 +1,60 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import {
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material'
 import { useState } from 'react'
 
 export type TypeFilterProps = {}
 
 function TypeFilter({}: TypeFilterProps) {
-  const [type, setType] = useState('')
+  const [type, setType] = useState<string[]>([])
+
+  const ITEM_HEIGHT = 48
+  const ITEM_PADDING_TOP = 8
+
+  const types = ['Hoodies', 'Sweaters', 'Shirts', 'Pants', 'Jackets']
+
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  }
+  const handleChange = (e: SelectChangeEvent<typeof type>) => {
+    const {
+      target: { value },
+    } = e
+    setType(
+      // On autofill we get a the stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    )
+  }
   return (
     <FormControl sx={{ minWidth: 80 }}>
-      {!type && (
-        <InputLabel id='typeFilter' shrink={false}>
-          Type
-        </InputLabel>
-      )}
-      <Select
+      <InputLabel
+        id='typeFilter'
+        sx={
+          type.length > 0
+            ? {
+                height: '2rem !important',
+                fontWeight: 'bold !important',
+                margin: '0 -0.641px !important',
+              }
+            : {}
+        }
+      >
+        Type
+      </InputLabel>
+
+      {/* <Select
         labelId='typeFilter'
         value={type}
         sx={{ height: 50 }}
@@ -23,6 +65,23 @@ function TypeFilter({}: TypeFilterProps) {
         <MenuItem value='shirts'>Shirts</MenuItem>
         <MenuItem value='pants'>Pants</MenuItem>
         <MenuItem value='jackets'>Jackets</MenuItem>
+      </Select> */}
+      <Select
+        labelId='typeFilter'
+        multiple
+        value={type}
+        onChange={handleChange}
+        input={<OutlinedInput label='Type' />}
+        // renderValue={(selected) => selected.join(', ')}
+        renderValue={() => ''}
+        MenuProps={MenuProps}
+      >
+        {types.map((type_) => (
+          <MenuItem key={type_} value={type_}>
+            <Checkbox checked={type.indexOf(type_) > -1} />
+            <ListItemText primary={type_} />
+          </MenuItem>
+        ))}
       </Select>
     </FormControl>
   )
