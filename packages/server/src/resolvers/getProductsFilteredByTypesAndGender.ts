946b79cac5ba7c93e3195d8e61eb5d5face9e290
Kevin Baik <mphist@gmail.com>
2021-12-04 20:19:54 -0500

    Server - fixed bug in resolver for filter

--- packages/server/src/resolvers/getProductsFilteredByTypesAndGender.ts
+++ packages/server/src/resolvers/getProductsFilteredByTypesAndGender.ts
@@ -1,6 +1,11 @@
 import { Product } from '.prisma/client'
 import { Context } from '../utils/context'
 
+const parseType = (str: string) => {
+  const type = str.replace(' & ', '&').replace(/ /g, '_').toLowerCase()
+  return type
+}
+
 export const getProductsFilteredByTypesAndGender = async (
   types: string[],
   gender: string,
@@ -8,17 +13,20 @@ export const getProductsFilteredByTypesAndGender = async (
 ) => {
   try {
     const items: Product[][] = []
-    types.forEach(async (type) => {
-      const tempItems = await prisma.product.findMany({
-        where: {
-          type,
-          gender: gender === 'W' ? 'F' : 'M',
-        },
-      })
-      items.push(tempItems)
-    })
 
-    console.log(items)
+    await Promise.all(
+      types.map(async (type) => {
+        const tempItems = await prisma.product.findMany({
+          where: {
+            category: parseType(type),
+            gender: gender === 'W' ? 'F' : 'M',
+          },
+        })
+        items.push(tempItems)
+      })
+    )
+
+    return items
   } catch (e) {
     console.log(e)
   }
