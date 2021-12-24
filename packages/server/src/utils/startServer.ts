a1b051871b33ad36dc23e8846518afad0784601e
Kevin Baik <mphist@gmail.com>
2021-12-23 20:03:19 -0500

    added introspection to apolloserver

--- packages/server/src/utils/startServer.ts
+++ packages/server/src/utils/startServer.ts
@@ -20,6 +20,7 @@ export const startServer = async () => {
     typeDefs,
     resolvers,
     context,
+    introspection: true,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
   })
 
